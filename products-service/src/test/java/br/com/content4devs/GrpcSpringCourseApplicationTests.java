d92203a3d1a84d5590e8a599aab19c025684ddfc
tonyals <tony_luzsilva@hotmail.com>
2021-12-06 08:18:52 -0300

    feat: hello world grpc

---
+++ products-service/src/test/java/br/com/content4devs/GrpcSpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.content4devs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSpringCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
