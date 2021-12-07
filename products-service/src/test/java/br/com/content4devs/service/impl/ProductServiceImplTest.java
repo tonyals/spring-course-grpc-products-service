af7fa39d9fa9b563d18146e585cad5c61665b932
tonyals <tony_luzsilva@hotmail.com>
2021-12-07 08:06:31 -0300

    feat: implements test for create service method

---
+++ products-service/src/test/java/br/com/content4devs/service/impl/ProductServiceImplTest.java
@@ -0,0 +1,41 @@
+package br.com.content4devs.service.impl;
+
+import br.com.content4devs.domain.Product;
+import br.com.content4devs.dto.ProductInputDTO;
+import br.com.content4devs.dto.ProductOutputDTO;
+import br.com.content4devs.repository.ProductRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceImplTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductServiceImpl productService;
+
+    @Test
+    @DisplayName("when create product service is call with valid data a product is returned")
+    public void createProductSuccessTest() {
+        Product product = new Product(1L, "product name", 10.00, 10);
+
+        when(productRepository.save(any())).thenReturn(product);
+
+        ProductInputDTO inputDTO = new ProductInputDTO("product name", 10.00, 10);
+        ProductOutputDTO outputDTO = productService.create(inputDTO);
+
+        assertThat(outputDTO)
+                .usingRecursiveComparison()
+                .isEqualTo(product);
+    }
+}
