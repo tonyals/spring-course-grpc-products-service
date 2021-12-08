53abe61c464ccae8e26ef646639c55bbc1537011
tonyals <tony_luzsilva@hotmail.com>
2021-12-08 12:31:28 -0300

    feat: implements integration test for AlreadyExistsException

--- products-service/src/test/java/br/com/content4devs/resources/ProductResourceIntegrationTest.java
+++ products-service/src/test/java/br/com/content4devs/resources/ProductResourceIntegrationTest.java
@@ -3,10 +3,14 @@ package br.com.content4devs.resources;
 import br.com.content4devs.ProductRequest;
 import br.com.content4devs.ProductResponse;
 import br.com.content4devs.ProductServiceGrpc;
+import io.grpc.StatusRuntimeException;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.assertj.core.api.Assertions;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
@@ -18,6 +22,15 @@ public class ProductResourceIntegrationTest {
 
     @GrpcClient("inProcess")
     private ProductServiceGrpc.ProductServiceBlockingStub serviceBlockingStub;
+
+    @Autowired
+    private Flyway flyway;
+
+    @BeforeEach
+    public void setUp() {
+        flyway.clean();
+        flyway.migrate();
+    }
 
     @Test
     @DisplayName("when valid data is provided a product is created")
@@ -33,5 +46,20 @@ public class ProductResourceIntegrationTest {
                 .usingRecursiveComparison()
                 .comparingOnlyFields("name", "price", "quantity_in_stock")
                 .isEqualTo(productResponse);
+    }
+
+    @Test
+    @DisplayName("when create is called with duplicated name, throw ProductAlreadyExistsException")
+    public void createProductAlreadyExistsExceptionTest() {
+        ProductRequest productRequest = ProductRequest.newBuilder()
+                .setName("Product A")
+                .setPrice(10.00)
+                .setQuantityInStock(100)
+                .build();
+
+        Assertions.assertThatExceptionOfType(StatusRuntimeException.class)
+                .isThrownBy(() -> serviceBlockingStub.create(productRequest))
+                .withMessage("ALREADY_EXISTS: Produto Product A já cadastrado no sistema.");
+
     }
 }
